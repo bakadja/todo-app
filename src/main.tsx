1f8a84817767c95998f3e019cf3d930bdcf5d208
Kevin Ngongang <70435295+bakadja@users.noreply.github.com>
2026-09-04 22:33:59 +0200

    feat: provide authentication context

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
 import App from "./App.tsx";
+import { AuthProvider } from "./auth/AuthContext";
 import "./index.css";
 
 registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 );
